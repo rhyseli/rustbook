e53ec58e30f448d9107984b3b7cf40a4e3db5ddc
Elliott Rhys <hello@elliottrhys.com>
2021-08-31 23:27:42 +0100

    initial commit

---
+++ hello_cargo/target/debug/deps/hello_cargo-0ed49cb6e5fd88e6.d
@@ -0,0 +1,5 @@
+C:\Users\rhyse\dev\rustbook\hello_cargo\target\debug\deps\hello_cargo-0ed49cb6e5fd88e6.rmeta: src\main.rs
+
+C:\Users\rhyse\dev\rustbook\hello_cargo\target\debug\deps\hello_cargo-0ed49cb6e5fd88e6.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\rhyse\dev\rustbook\hello_cargo\target\debug\hello_cargo.exe: C:\Users\rhyse\dev\rustbook\hello_cargo\src\main.rs
